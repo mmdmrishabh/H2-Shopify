efe85825bee91540863d1255dc4a2f72d0c8c5c8
mmdmrishabh <rishabh@marmeto.com>
2022-12-25 03:09:44 +0530

    added gql collection

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,12 +1,16 @@
+import { Suspense } from "react";
+import { Layout } from "../components/Layout.server";
+import FeaturedCollections from "../components/FeaturedCollections.server";
 export default function Home() {
   return (
-    <section className="p-6 md:p-8 lg:p-12 text-center">
-    <h1 className="font-extrabold mb-4 text-5xl md:text-7xl">Hello world!</h1>
-    <p className="font-bold mb-3">Welcome to Hydrogen.</p>
-    <p>
-      Hydrogen is a front-end web development framework used for building
-      Shopify custom storefronts.
-    </p>
-  </section>
-  )
+    <Layout>
+      <div className="p-6 md:p-8 lg:p-12 text-center">
+        <p className="font-bold mb-3">Day 1</p>
+      </div>
+        
+      <Suspense>
+      <FeaturedCollections />
+      </Suspense>
+    </Layout>
+  );
 }
