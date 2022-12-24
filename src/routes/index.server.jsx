3ae7ff8915bea6d972c387afa6c61c87cff98344
mmdmrishabh <rishabh@marmeto.com>
2022-12-25 01:17:15 +0530

    added inital files

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,5 +1,12 @@
 export default function Home() {
   return (
-    <div className="header">Header</div>
+    <section className="p-6 md:p-8 lg:p-12 text-center">
+    <h1 className="font-extrabold mb-4 text-5xl md:text-7xl">Hello world!</h1>
+    <p className="font-bold mb-3">Welcome to Hydrogen.</p>
+    <p>
+      Hydrogen is a front-end web development framework used for building
+      Shopify custom storefronts.
+    </p>
+  </section>
   )
 }
